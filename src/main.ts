ba6c9b39c756b71d21e7fa261d2e57e5c6314cdc
Atte Kivimaki <aj.kivimaki@gmail.com>
2024-01-30 18:20:59 +0200

    add logic, clean the code, divide to helper functions, add documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,92 @@
+// DOM elements
+const screenEl = document.querySelector("#screen") as HTMLDivElement;
 const buttonElems = document.querySelectorAll("button");
 
-buttonElems.forEach((button) =>
-  button.addEventListener("click", (e) => handleClick(e))
-);
+// Event Listeners
+buttonElems.forEach((button) => button.addEventListener("click", handleClick));
 
-function handleClick(e: Event): void {
-  console.log((e.target as HTMLInputElement).value);
+// Variables
+let screenValueString = "0.00";
+let expressionString = "0.00";
+let lastChar = "";
+let result = "";
+
+// Initial screen value
+screenEl.textContent = screenValueString;
+
+// Calculate and display result based on button click ( + | - | / | * )
+function calc(char: string) {
+  screenEl.textContent = result;
+  return (expressionString = result + char);
+}
+
+// Handle button click event
+function handleClick(e: Event) {
+  const button = (e.target as HTMLInputElement).value;
+
+  // Clear everything
+  if (button === "C") return resetCalc();
+
+  // Accept only number as a first character
+  if (
+    (expressionString === "0.00" && button === "/") ||
+    (expressionString === "0.00" && button === "*") ||
+    (expressionString === "0.00" && button === "=") ||
+    (expressionString === "0.00" && button === "-") ||
+    (expressionString === "0.00" && button === "=") ||
+    (expressionString === "0.00" && button === "+")
+  )
+    return;
+
+  // If not a number, calculate and show result
+  if (
+    button === "/" ||
+    button === "*" ||
+    button === "-" ||
+    button === "+" ||
+    button === "="
+  ) {
+    // Does not accept subsequent arithmetic opartion signs
+    if (button === lastChar) return;
+    lastChar = button;
+
+    // evaluate the math expression string
+    result = eval(expressionString).toFixed(2).toString();
+
+    if (button === "=") {
+      updateScreenValue(result);
+      expressionString = result;
+    } else {
+      calc(button);
+    }
+  }
+
+  // If a number, add to the math expression string
+  else {
+    lastChar = button;
+
+    // Do not add extra 0 to 0.00 value
+    if (expressionString === "0.00" && button === "0") return;
+
+    if (expressionString === "0.00") {
+      expressionString = button;
+      updateScreenValue(button);
+    } else {
+      expressionString += button;
+      updateScreenValue(expressionString);
+    }
+  }
+}
+
+// Reset the calculator to initial state
+function resetCalc() {
+  expressionString = "0.00";
+  screenValueString = "0.00";
+  updateScreenValue(screenValueString);
+}
+
+// Set screen value and update the DOM
+function updateScreenValue(value: string) {
+  screenValueString = value;
+  screenEl.textContent = screenValueString;
 }
